0339a9ded9940c828bf190d746e9174a5005a80c
Diego <dieguito.mena@gmail.com>
2021-04-07 11:32:10 -0400

    Fixes

--- index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var _wizardform = require('./wizardform');
-
-var _wizardform2 = _interopRequireDefault(_wizardform);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
